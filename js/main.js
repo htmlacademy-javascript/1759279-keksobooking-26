373c255c2839e4f62e265abdd03009fe08f710e4
Daria Nadosha <daria.nadosha@gmail.com>
2022-06-14 22:22:26 +0300

    исправлены ошибки линтера в функциях

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
-const GetRandomInteger = (min, max) => {
+const getRandomInteger = (min, max) => {
   // min = Math.ceil(min);
   // max = Math.floor(max);
   if (max <= min || min < 0 || max <= 0 ) {
     return ('Функция не может быть выполнена');
   }
-    return Math.floor(Math.random() * (max - min)) + min;
-}
-GetRandomInteger(1,100)
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+getRandomInteger(1,100);
 
-const GetRandomIntegerFloating = (min, max, digits) => {
+const getRandomIntegerFloating = (min, max, digits) => {
   // min = Math.ceil(min);
   // max = Math.floor(max);
   if (max <= min || min < 0 || max <= 0 ) {
     return ('Функция не может быть выполнена');
   }
-    const digitsDegree = 10 ** digits;
-    return Math.floor((Math.random() * (max - min)) + min * digitsDegree)/digitsDegree;
-}
-GetRandomIntegerFloating(0,100,3)
+  const digitsDegree = 10 ** digits;
+  return Math.floor((Math.random() * (max - min)) + min * digitsDegree)/digitsDegree;
+};
+getRandomIntegerFloating(0,100,3);
