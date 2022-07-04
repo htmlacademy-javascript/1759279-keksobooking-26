1075fdef062c69f60df76dfbd2f2e7f38328c379
Daria Nadosha <daria.nadosha@gmail.com>
2022-07-04 18:25:33 +0300

    исправлена функция для отрисовки карточки

--- js/create-elements.js
+++ js/create-elements.js
@@ -7,9 +7,9 @@ const similarAdFragment = document.createDocumentFragment();
 
 const renderCards = (offer) => {
   const adElement = similarAdTemplate.cloneNode(true);
-  adElement.querySelector('.popup__title').textContent = offer.title;
-  adElement.querySelector('.popup__text--address').textContent = offer.address;
-  adElement.querySelector('.popup__text--price').textContent = `${offer.price} ₽/ночь`;
+  adElement.querySelector('.popup__title').textContent = `${offer.offer.title}`;
+  adElement.querySelector('.popup__text--address').textContent = `${offer.offer.address}`;
+  adElement.querySelector('.popup__text--price').textContent = `${offer.offer.price} ₽/ночь`;
 
   similarAdFragment.append(adElement);
   similarAdElement.append(similarAdFragment);
--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 import {generateDataOffers} from './data.js';
 
-// generateDataOffers(10);
+generateDataOffers(10);
 
 import {renderCards} from './create-elements.js';
 const dataOffers = generateDataOffers(10);
 renderCards(dataOffers[0]);
 
-// console.log(getSimilarOffers(10));
+// console.log(generateDataOffers(10));
