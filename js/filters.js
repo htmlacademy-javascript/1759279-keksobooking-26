3a44f29729b1449ce5f84d603219c9be9f707e4f
Daria Nadosha <daria.nadosha@gmail.com>
2022-07-24 23:59:48 +0300

    выполнено задание по получению данных с сервера

---
+++ js/filters.js
@@ -0,0 +1,12 @@
+// const CARDS_COUNT = 10;
+// const Price = {
+//   MIDDLE: 10000,
+//   HIGH: 50000,
+// };
+
+// const filterMap = document.querySelector('.map__filters');
+// const filterType = filterMap.querySelector('#housing-type');
+// const filterPrice = filterMap.querySelector('#housing-price');
+// const filterRooms = filterMap.querySelector('#housing-rooms');
+// const filterGuests = filterMap.querySelector('#housing-guests');
+// const filterFeatures = filterMap.querySelectorAll('.map__checkbox');
